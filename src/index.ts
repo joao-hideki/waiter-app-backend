ec2caefff2f8e0f1019331e2e788536b124a981f
joao-hideki <joaoohideki12345@gmail.com>
2023-03-11 03:53:48 -0300

    disponibilizando as imagens da pasta uploads através do static paths

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import {config} from 'dotenv';
 import { router } from './router';
+import path from 'node:path';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     console.log('connected to mongodb');
     config();
     const app = express();
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
     app.listen(process.env.PORT, () => {
