db55bf32924e08e17b6e629b8941c77fa7db4dc4
joao-hideki <joaoohideki12345@gmail.com>
2023-03-10 22:54:11 -0300

    estabelecendo conexão com o mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {config} from 'dotenv';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    console.log('connected to mongodb');
+    config();
+    const app = express();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch(() => console.log('error connecting to mongo'));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
