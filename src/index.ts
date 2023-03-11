b4727aa4cc1e27964e0bd1d64be5fd6bd59a92d4
joao-hideki <joaoohideki12345@gmail.com>
2023-03-10 23:36:43 -0300

    setando as rotas da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import {config} from 'dotenv';
+import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     console.log('connected to mongodb');
     config();
     const app = express();
+    app.use(express.json());
+    app.use(router);
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on http://localhost:${process.env.PORT}`);
     });
