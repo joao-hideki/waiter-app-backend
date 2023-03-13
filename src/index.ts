7f5859160a997a10d368f71243b9c6023f79aca8
joao-hideki <joaoohideki12345@gmail.com>
2023-03-13 02:40:59 -0300

    habilitando protocolo websocket no servidor

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import mongoose from 'mongoose';
 import {config} from 'dotenv';
 import { router } from './router';
 import path from 'node:path';
+import http from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     console.log('connected to mongodb');
     config();
-    const app = express();
+
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
       res.setHeader('Access-Control-Allow-Methods', '*');
@@ -18,7 +24,8 @@ mongoose.connect('mongodb://localhost:27017')
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
-    app.listen(process.env.PORT, () => {
+
+    server.listen(process.env.PORT, () => {
       console.log(`Server is running on http://localhost:${process.env.PORT}`);
     });
   })
